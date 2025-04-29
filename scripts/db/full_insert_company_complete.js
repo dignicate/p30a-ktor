8b769538520f5227c8fa39c07c1b110590f296de
xinyiqiw <shinichi_watanabe@dignicate.com>
2025-04-29 21:12:49 +0900

    database scripts.

---
+++ scripts/db/full_insert_company_complete.js
@@ -0,0 +1,625 @@
+db.company.insertMany(
+[
+  {
+    "_id": 1,
+    "name": [
+      {
+        "language": "en",
+        "value": "Toyota"
+      },
+      {
+        "language": "ja",
+        "value": "トヨタ"
+      }
+    ],
+    "countryId": 1,
+    "foundedYear": 1937
+  },
+  {
+    "_id": 2,
+    "name": [
+      {
+        "language": "en",
+        "value": "Ford"
+      },
+      {
+        "language": "ja",
+        "value": "フォード"
+      }
+    ],
+    "countryId": 3,
+    "foundedYear": 1903
+  },
+  {
+    "_id": 3,
+    "name": [
+      {
+        "language": "en",
+        "value": "BMW"
+      },
+      {
+        "language": "ja",
+        "value": "ビー・エム・ダブリュー"
+      },
+      {
+        "language": "de",
+        "value": "BMW"
+      }
+    ],
+    "countryId": 2,
+    "foundedYear": 1916
+  },
+  {
+    "_id": 4,
+    "name": [
+      {
+        "language": "en",
+        "value": "Hyundai"
+      },
+      {
+        "language": "ja",
+        "value": "ヒュンダイ"
+      },
+      {
+        "language": "ko",
+        "value": "현대"
+      }
+    ],
+    "countryId": 4,
+    "foundedYear": 1967
+  },
+  {
+    "_id": 5,
+    "name": [
+      {
+        "language": "en",
+        "value": "Fiat"
+      },
+      {
+        "language": "ja",
+        "value": "フィアット"
+      },
+      {
+        "language": "it",
+        "value": "Fiat"
+      }
+    ],
+    "countryId": 5,
+    "foundedYear": 1899
+  },
+  {
+    "_id": 6,
+    "name": [
+      {
+        "language": "en",
+        "value": "Volvo"
+      },
+      {
+        "language": "ja",
+        "value": "ボルボ"
+      },
+      {
+        "language": "sv",
+        "value": "Volvo"
+      }
+    ],
+    "countryId": 6,
+    "foundedYear": 1927
+  },
+  {
+    "_id": 7,
+    "name": [
+      {
+        "language": "en",
+        "value": "Tata Motors"
+      },
+      {
+        "language": "ja",
+        "value": "タタ・モーターズ"
+      },
+      {
+        "language": "hi",
+        "value": "टाटा मोटर्स"
+      }
+    ],
+    "countryId": 7,
+    "foundedYear": 1945
+  },
+  {
+    "_id": 8,
+    "name": [
+      {
+        "language": "en",
+        "value": "Renault"
+      },
+      {
+        "language": "ja",
+        "value": "ルノー"
+      },
+      {
+        "language": "fr",
+        "value": "Renault"
+      }
+    ],
+    "countryId": 8,
+    "foundedYear": 1899
+  },
+  {
+    "_id": 9,
+    "name": [
+      {
+        "language": "en",
+        "value": "Koenigsegg"
+      },
+      {
+        "language": "ja",
+        "value": "ケーニグセグ"
+      }
+    ],
+    "countryId": 6,
+    "foundedYear": 1994
+  },
+  {
+    "_id": 10,
+    "name": [
+      {
+        "language": "en",
+        "value": "Pagani"
+      },
+      {
+        "language": "ja",
+        "value": "パガーニ"
+      },
+      {
+        "language": "it",
+        "value": "Pagani"
+      }
+    ],
+    "countryId": 5,
+    "foundedYear": 1992
+  },
+  {
+    "_id": 11,
+    "name": [
+      {
+        "language": "en",
+        "value": "Rimac"
+      },
+      {
+        "language": "ja",
+        "value": "リマック"
+      },
+      {
+        "language": "hr",
+        "value": "Rimac"
+      }
+    ],
+    "countryId": 9,
+    "foundedYear": 2009
+  },
+  {
+    "_id": 12,
+    "name": [
+      {
+        "language": "en",
+        "value": "Zastava"
+      },
+      {
+        "language": "ja",
+        "value": "ザスタバ"
+      },
+      {
+        "language": "sr",
+        "value": "Застава"
+      }
+    ],
+    "countryId": 10,
+    "foundedYear": 1953
+  },
+  {
+    "_id": 13,
+    "name": [
+      {
+        "language": "en",
+        "value": "Holden"
+      },
+      {
+        "language": "ja",
+        "value": "ホールデン"
+      }
+    ],
+    "countryId": 11,
+    "foundedYear": 1856
+  },
+  {
+    "_id": 14,
+    "name": [
+      {
+        "language": "en",
+        "value": "Saab"
+      },
+      {
+        "language": "ja",
+        "value": "サーブ"
+      }
+    ],
+    "countryId": 6,
+    "foundedYear": 1945
+  },
+  {
+    "_id": 15,
+    "name": [
+      {
+        "language": "en",
+        "value": "Peugeot"
+      },
+      {
+        "language": "ja",
+        "value": "プジョー"
+      },
+      {
+        "language": "fr",
+        "value": "Peugeot"
+      }
+    ],
+    "countryId": 8,
+    "foundedYear": 1810
+  },
+  {
+    "_id": 16,
+    "name": [
+      {
+        "language": "en",
+        "value": "Mitsuoka"
+      },
+      {
+        "language": "ja",
+        "value": "光岡自動車"
+      }
+    ],
+    "countryId": 1,
+    "foundedYear": 1968
+  },
+  {
+    "_id": 17,
+    "name": [
+      {
+        "language": "en",
+        "value": "Pyeonghwa Motors"
+      },
+      {
+        "language": "ja",
+        "value": "平和自動車"
+      },
+      {
+        "language": "ko",
+        "value": "평화자동차"
+      }
+    ],
+    "countryId": 12,
+    "foundedYear": 1999
+  },
+  {
+    "_id": 18,
+    "name": [
+      {
+        "language": "en",
+        "value": "Iran Khodro"
+      },
+      {
+        "language": "ja",
+        "value": "イラン・ホドロ"
+      },
+      {
+        "language": "fa",
+        "value": "ایران خودرو"
+      }
+    ],
+    "countryId": 13,
+    "foundedYear": 1962
+  },
+  {
+    "_id": 19,
+    "name": [
+      {
+        "language": "en",
+        "value": "State Company for Automotive Industry"
+      },
+      {
+        "language": "ja",
+        "value": "国営自動車産業会社"
+      },
+      {
+        "language": "ar",
+        "value": "الشركة العامة لصناعة السيارات"
+      }
+    ],
+    "countryId": 14,
+    "foundedYear": 1976
+  },
+  {
+    "_id": 20,
+    "name": [
+      {
+        "language": "en",
+        "value": "Syria Motors"
+      },
+      {
+        "language": "ja",
+        "value": "シリア・モーターズ"
+      },
+      {
+        "language": "ar",
+        "value": "سوريا موتورز"
+      }
+    ],
+    "countryId": 15,
+    "foundedYear": 2007
+  },
+  {
+    "_id": 21,
+    "name": [
+      {
+        "language": "en",
+        "value": "Suzuki"
+      },
+      {
+        "language": "ja",
+        "value": "スズキ"
+      }
+    ],
+    "countryId": 1,
+    "foundedYear": 1909
+  },
+  {
+    "_id": 22,
+    "name": [
+      {
+        "language": "en",
+        "value": "Mazda"
+      },
+      {
+        "language": "ja",
+        "value": "マツダ"
+      }
+    ],
+    "countryId": 1,
+    "foundedYear": 1920
+  },
+  {
+    "_id": 23,
+    "name": [
+      {
+        "language": "en",
+        "value": "Subaru"
+      },
+      {
+        "language": "ja",
+        "value": "スバル"
+      }
+    ],
+    "countryId": 1,
+    "foundedYear": 1953
+  },
+  {
+    "_id": 24,
+    "name": [
+      {
+        "language": "en",
+        "value": "Mitsubishi"
+      },
+      {
+        "language": "ja",
+        "value": "三菱"
+      }
+    ],
+    "countryId": 1,
+    "foundedYear": 1870
+  },
+  {
+    "_id": 25,
+    "name": [
+      {
+        "language": "en",
+        "value": "Isuzu"
+      },
+      {
+        "language": "ja",
+        "value": "いすゞ"
+      }
+    ],
+    "countryId": 1,
+    "foundedYear": 1916
+  },
+  {
+    "_id": 26,
+    "name": [
+      {
+        "language": "en",
+        "value": "AvtoVAZ"
+      },
+      {
+        "language": "ja",
+        "value": "アフトヴァズ"
+      },
+      {
+        "language": "ru",
+        "value": "АвтоВАЗ"
+      }
+    ],
+    "countryId": 16,
+    "foundedYear": 1966
+  },
+  {
+    "_id": 27,
+    "name": [
+      {
+        "language": "en",
+        "value": "GAZ"
+      },
+      {
+        "language": "ja",
+        "value": "ガズ"
+      },
+      {
+        "language": "ru",
+        "value": "ГАЗ"
+      }
+    ],
+    "countryId": 16,
+    "foundedYear": 1932
+  },
+  {
+    "_id": 28,
+    "name": [
+      {
+        "language": "en",
+        "value": "ZAZ"
+      },
+      {
+        "language": "ja",
+        "value": "ザズ"
+      },
+      {
+        "language": "uk",
+        "value": "ЗАЗ"
+      }
+    ],
+    "countryId": 17,
+    "foundedYear": 1923
+  },
+  {
+    "_id": 29,
+    "name": [
+      {
+        "language": "en",
+        "value": "BelAZ"
+      },
+      {
+        "language": "ja",
+        "value": "ベラズ"
+      },
+      {
+        "language": "be",
+        "value": "БелАЗ"
+      }
+    ],
+    "countryId": 18,
+    "foundedYear": 1948
+  },
+  {
+    "_id": 30,
+    "name": [
+      {
+        "language": "en",
+        "value": "UAZ"
+      },
+      {
+        "language": "ja",
+        "value": "ウアズ"
+      },
+      {
+        "language": "ru",
+        "value": "УАЗ"
+      }
+    ],
+    "countryId": 16,
+    "foundedYear": 1941
+  },
+  {
+    "_id": 31,
+    "name": [
+      {
+        "language": "en",
+        "value": "Autocars Co."
+      },
+      {
+        "language": "he",
+        "value": "אוטוקרס"
+      },
+      {
+        "language": "ja",
+        "value": "オートカーズ"
+      }
+    ],
+    "countryId": 19,
+    "foundedYear": 1957
+  },
+  {
+    "_id": 32,
+    "name": [
+      {
+        "language": "en",
+        "value": "Wallyscar"
+      },
+      {
+        "language": "fr",
+        "value": "Wallyscar"
+      },
+      {
+        "language": "ja",
+        "value": "ウォリースカー"
+      }
+    ],
+    "countryId": 20,
+    "foundedYear": 2006
+  },
+  {
+    "_id": 33,
+    "name": [
+      {
+        "language": "en",
+        "value": "Innoson Vehicle Manufacturing"
+      },
+      {
+        "language": "ig",
+        "value": "Innoson Vehicle Manufacturing"
+      },
+      {
+        "language": "ja",
+        "value": "イノソン・ビークル・マニュファクチャリング"
+      }
+    ],
+    "countryId": 21,
+    "foundedYear": 2007
+  },
+  {
+    "_id": 34,
+    "name": [
+      {
+        "language": "en",
+        "value": "Troller"
+      },
+      {
+        "language": "pt",
+        "value": "Troller"
+      },
+      {
+        "language": "ja",
+        "value": "トローラー"
+      }
+    ],
+    "countryId": 22,
+    "foundedYear": 1995
+  },
+  {
+    "_id": 35,
+    "name": [
+      {
+        "language": "en",
+        "value": "Zanella"
+      },
+      {
+        "language": "es",
+        "value": "Zanella"
+      },
+      {
+        "language": "ja",
+        "value": "ザネラ"
+      }
+    ],
+    "countryId": 23,
+    "foundedYear": 1948
+  }
+]
+);
