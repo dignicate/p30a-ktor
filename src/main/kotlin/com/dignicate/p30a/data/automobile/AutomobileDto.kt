0479da85a0977d41e7630e17a11ad223cd740fc5
xinyiqiw <shinichi_watanabe@dignicate.com>
2025-04-29 21:26:02 +0900

    succeed to access database.

---
+++ src/main/kotlin/com/dignicate/p30a/data/automobile/AutomobileDto.kt
@@ -0,0 +1,10 @@
+package com.dignicate.p30a.data.automobile
+
+import com.dignicate.p30a.domain.automobile.LocalizedString
+
+data class CompanyDto(
+    val _id: Int,
+    val name: List<LocalizedString>,
+    val countryId: Int,
+    val foundedYear: Int
+)
--- src/main/kotlin/com/dignicate/p30a/data/automobile/AutomobileRepositoryImpl.kt
+++ src/main/kotlin/com/dignicate/p30a/data/automobile/AutomobileRepositoryImpl.kt
@@ -9,6 +9,20 @@ class AutomobileRepositoryImpl(
 ) : AutomobileRepository {
 
     override suspend fun getCompanies(limit: Int, page: Int): List<Company> {
-        return mongoDbClient.findAll("companies", Company::class.java, limit, page)
+        return try {
+            mongoDbClient
+                .findAll("company", CompanyDto::class.java, limit, page)
+                .map { it.toDomain() }
+        } catch (e: Exception) {
+            println("MongoDB error: ${e.message}")
+            throw e
+        }
     }
 }
+
+private fun CompanyDto.toDomain(): Company = Company(
+    id = _id,
+    name = name,
+    country = emptyList(), // todo:
+    foundedYear = foundedYear
+)
