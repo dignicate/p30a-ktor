7f49455dbe84dd96938ffc6f1449d91b8c2a259f
xinyiqi <57293323+Xinyiqiw@users.noreply.github.com>
2025-05-03 15:18:49 +0900

    Fix OpenAPI generation compatibility (#11)
    * empty commit
    
    * fix for openapi.

--- src/main/kotlin/com/dignicate/p30a/controller/automobile/AutomobileController.kt
+++ src/main/kotlin/com/dignicate/p30a/controller/automobile/AutomobileController.kt
@@ -1,15 +1,12 @@
 package com.dignicate.p30a.controller.automobile
 
-import com.dignicate.p30a.controller.ApplicationCallWrapper
 import com.dignicate.p30a.domain.automobile.Company
 import com.dignicate.p30a.domain.automobile.GetCompaniesUseCase
 
 class AutomobileController(
-    private val call: ApplicationCallWrapper<List<Company>>,
     private val getCompaniesUseCase: GetCompaniesUseCase
 ) {
-    suspend fun getCompanies(limit: Int, page: Int) {
-        val companies = getCompaniesUseCase.execute(limit, page)
-        call.respond(companies)
+    suspend fun getCompanies(limit: Int, page: Int): List<Company> {
+        return getCompaniesUseCase.execute(limit, page)
     }
 }
