21bcfd3902d41928cac9b5d974bac8872d1c1ed1
xinyiqiw <shinichi_watanabe@dignicate.com>
2026-04-05 16:08:25 +0900

    Update devctl and up.sh to improve service startup process

--- scripts/module/up.sh
+++ scripts/module/up.sh
@@ -3,5 +3,8 @@
 # shellcheck disable=SC1091
 source "$(dirname "$0")/common.sh"
 
+echo "Building the application..."
+./gradlew installDist
+
 echo "Starting services..."
 docker compose up --build -d
